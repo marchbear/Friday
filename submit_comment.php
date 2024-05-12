06e57ef5c3e41b8856a57e73e06346905eaa8c56
Szu-Wen <168436676+Szu-Wen@users.noreply.github.com>
2024-05-12 23:36:50 +0800

    Add files via upload

---
+++ submit_comment.php
@@ -0,0 +1,32 @@
+<?php
+include 'db_connection.php';
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // 接收表單提交的資料
+    $userid =  $_SESSION['user_id'];
+
+
+    //驗證並過濾用戶輸入的標題和內容 、 輸出轉義
+    //FILTER_SANITIZE_STRING 會過濾掉標記和其他不安全的字符，使輸入的内容變為纯文本，防止 XSS 攻擊。
+    $comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
+    $comment = htmlspecialchars($comment, ENT_QUOTES, 'UTF-8');
+
+    $articleid = $_SESSION['article_id'];
+    //使用參數化的 SQL 查詢
+    $sql = "INSERT INTO comments (article_id, author_id, content) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("iis", $articleid, $userid, $comment);
+
+    // 執行 SQL 查詢
+    if ($stmt->execute()) {
+         header("Location: article_content.php?id=$articleid");
+         exit(); // 重定向後結束程式，以確保不會繼續執行以下的代碼
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // 關閉 prepared statement 和資料庫連接
+    $stmt->close();
+    $conn->close();
+}
+?>
